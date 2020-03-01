f8a9925fd209951881e66b0a731e5d395bec56a3
ljc957468099 <38071481+ljc957468099@users.noreply.github.com>
2020-03-02 03:30:05 +0800

    Initial commit

---
+++ src/main/java/com/example/xmf/XmfApplication.java
@@ -0,0 +1,13 @@
+package com.example.xmf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xmf/XmfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
